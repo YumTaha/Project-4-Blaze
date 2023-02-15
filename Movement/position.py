1f4d583d1ee88f0a83a7e310b2a71f983598e42c
YumTaha <69929602+YumTaha@users.noreply.github.com>
2023-02-14 19:45:33 -0500

    idk

--- Movement/position.py
+++ Movement/position.py
@@ -1,15 +1,16 @@
-"""
-This module defines the functions for determining the position of a robot.
-"""
+
+#This module defines the functions for determining the position of a robot.
+
 
 from ev3dev2.sensor.lego import GyroSensor
 
 gyro = GyroSensor()
 
 def get_position():
-    """
-    Return the current position of the robot as a tuple of (x, y, angle).
-    """
+
+
+    #Return the current position of the robot as a tuple of (x, y, angle).
+
     # Implement your position tracking algorithm here using the GyroSensor and other sensors
     x = 0
     y = 0
