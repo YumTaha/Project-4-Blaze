44783490b7fec56a5d65da7f25a85fbb1dc62967
YumTaha <69929602+YumTaha@users.noreply.github.com>
2023-02-15 21:28:15 -0500

    .

--- Movement/Move.py
+++ Movement/Move.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # Import ev3 libraries
-from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D
 from time import sleep
 # Set up the motors for controlling the robot
-motor_left = LargeMotor(OUTPUT_B)
-motor_right = LargeMotor(OUTPUT_C)
+motor_left = LargeMotor(OUTPUT_A)
+motor_right = LargeMotor(OUTPUT_D)
 
 # Define the functions for moving the robot
 
--- Movement/position.py
+++ Movement/position.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, LargeMotor
+from ev3dev2.motor import OUTPUT_A, OUTPUT_D, LargeMotor
 
 def get_position(distance):
     motor_A = LargeMotor(OUTPUT_A)
-    motor_B = LargeMotor(OUTPUT_B)
+    motor_D = LargeMotor(OUTPUT_D)
 
     # Set the motor speed and run time based on the distance
     if distance > 0:
-        speed = 360
+        speed = 50
         run_time = (distance / 20) * 1000
     else:
-        speed = -360
+        speed = -50
         run_time = (-distance / 20) * 1000
 
     # Run the motors and wait for them to stop
     motor_A.on_for_seconds(speed=speed, seconds=run_time/1000)
-    motor_B.on_for_seconds(speed=speed, seconds=run_time/1000)
+    motor_D.on_for_seconds(speed=speed, seconds=run_time/1000)
 
     # Calculate the final position relative to the starting position
     final_x = 0
